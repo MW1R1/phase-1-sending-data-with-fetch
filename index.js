40e3d8480580d0f6030fa6ba2924c6c69b3d3776
MW1R1 <nickmwirigi58@gmail.com>
2023-04-01 14:05:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+const submitData = (myname, myemail) => {
+    let obj = {
+        name: myname,
+        email: myemail
+    };
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(obj)
+    })
+        .then(res => res.json())
+        .then(data => {
+            let userId = data.id;
+            let userIdElement = document.createElement("p");
+            userIdElement.textContent = userId;
+            document.body.appendChild(userIdElement);
+        })
+        .catch(error => {
+            let errorMessage = document.createElement('p')
+            errorMessage.textContent = error.message
+            document.body.append(errorMessage)
+        });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    submitData("John Doe", "johndoe@example.com");
+});
